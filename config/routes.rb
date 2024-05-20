32b96e4a87180ab13c17495132f319ed6550ab20
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-05-20 18:28:11 +0000

    fix update user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "users", :action => "home" })
+  get("/", { :controller => "users", :action => "index" })
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:username", { :controller => "users", :action => "show" })
   post("/insert_user_record", { :controller => "users", :action => "create" })
-  post("/update_users/:user_id", { :controller => "users", :action => "update" })
+  post("/update_user/:user_id", { :controller => "users", :action => "update" })
 
   get("/venues", { :controller => "venues", :action => "index" })
   post("/insert_venue_record", { :controller => "venues", :action => "create" })
