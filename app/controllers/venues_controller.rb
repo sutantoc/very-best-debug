bc17ca7c921a70732a078ad819eb35e8922cafd6
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-05-20 18:19:46 +0000

    fix update venue

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -26,15 +26,15 @@ class VenuesController < ApplicationController
   end
   
   def update
-    the_id = params.fetch("venue_id")
+    the_id = params.fetch("the_id")
 
-    @venue = Venue.where({ :id => the_id })
-    venue.address = params.fetch("query_address")
-    venue.name = params.fetch("Query_name")
-    venue.neighborhood = params.fetch("query_neighborhood")
-    venue.save
+    @venue = Venue.where({ :id => the_id }).at(0)
+    @venue.address = params.fetch("query_address")
+    @venue.name = params.fetch("query_name")
+    @venue.neighborhood = params.fetch("query_neighborhood")
+    @venue.save
     
-    redirect_to("/venues/#{venue.id}")
+    redirect_to("/venues/#{@venue.id}")
   end
 
   def destroy
